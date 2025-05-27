0b66867cd3c8bab45322578b40f27ef52253144e
Lance <enaganlance@gmail.com>
2025-05-28 00:54:34 +0800

    display header function

--- quiz_creator_to_oop/quiz_creator.py
+++ quiz_creator_to_oop/quiz_creator.py
@@ -1,3 +1,14 @@
+import random
+import time
+
 class QuizCreator:
+
     def __init__(self):
         self.welcome_messages = ["Let them suffer... hehe", "Challenge them!", "Welcome!"]
+
+    def display_header(self):
+        print("\n" + "-" * 50)
+        print(">>> QUIZ CREATOR <<<".center(50))
+        print("-" * 50)
+        print(random.choice(self.welcome_messages).center(50))
+        time.sleep(1)
