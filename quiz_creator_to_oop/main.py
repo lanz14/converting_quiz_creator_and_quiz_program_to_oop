50b48ca0f35711af1587b79197565a996e530a85
Lance <enaganlance@gmail.com>
2025-05-28 03:05:16 +0800

    from main, converted to only uses 1 class

--- quiz_creator_to_oop/main.py
+++ quiz_creator_to_oop/main.py
@@ -1,9 +1,6 @@
 from quiz_creator import QuizCreator
-from input_questions import InputQuestions
 
 quiz_creator = QuizCreator()
-questions = InputQuestions()
-
 quiz_creator.display_header()
-questions.generate_questions()
+quiz_creator.generate_questions()
 quiz_creator.save_quiz()
