9b18ae92d8a0e29ab59ccaa9d3662c35b6375df2
Lance <enaganlance@gmail.com>
2025-05-28 00:30:08 +0800

    add question function

--- quiz_creator_to_oop/input_questions.py
+++ quiz_creator_to_oop/input_questions.py
@@ -5,3 +5,11 @@ class InputQuestions:
 
     def get_question_input(self):
         question = input("Input your question (type 'e' to exit): ")
+        return question
+
+    def add_question(self):
+        print("\n=== NEW QUESTION ===")
+        
+        question_text = self.get_question_input()
+        if question_text.lower() == 'e':
+            return False
