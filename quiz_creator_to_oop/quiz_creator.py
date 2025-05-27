344c555f020e5decd29a978ad0fde4815377ca6a
Lance <enaganlance@gmail.com>
2025-05-28 01:30:38 +0800

    filename function

--- quiz_creator_to_oop/quiz_creator.py
+++ quiz_creator_to_oop/quiz_creator.py
@@ -3,8 +3,9 @@ import time
 
 class QuizCreator:
 
-    def __init__(self):
+    def __init__(self, quiz):
         self.welcome_messages = ["Let them suffer... hehe", "Challenge them!", "Welcome!"]
+        self.quiz = []
 
     def display_header(self):
         print("\n" + "-" * 50)
@@ -12,3 +13,11 @@ class QuizCreator:
         print("-" * 50)
         print(random.choice(self.welcome_messages).center(50))
         time.sleep(1)
+
+    def filename(self):
+        filename = input("\nName your quiz file (press Enter for 'my_quiz.txt'): ")
+        if not filename:
+            filename = "my_quiz.txt"
+        if not filename.endswith('.txt'):
+            filename += '.txt'
+        return filename
