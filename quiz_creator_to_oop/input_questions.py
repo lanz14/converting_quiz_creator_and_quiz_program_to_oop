a1492e0ed8d2cf24766a79c8ad51b11314a8a049
Lance <enaganlance@gmail.com>
2025-05-28 00:18:24 +0800

    class for user's input questions

---
+++ quiz_creator_to_oop/input_questions.py
@@ -0,0 +1,7 @@
+class InputQuestions:
+
+    def __init__(self, question):
+        self.question = question
+
+    def get_question_input(self):
+        question = input("Input your question (type 'e' to exit): ")
