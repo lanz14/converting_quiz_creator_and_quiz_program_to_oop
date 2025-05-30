746f68a6a214e5510dc61064745a391c7984f110
Lance <enaganlance@gmail.com>
2025-05-30 21:47:57 +0800

    display questions and choices method

--- quiz_program_to_oop/question.py
+++ quiz_program_to_oop/question.py
@@ -2,3 +2,10 @@ class Question:
     def __init__(self, question_text, choices):
         self.question_text = question_text
         self.choices = choices
+
+    def display_questions_choices(self, question_number):
+        print(f"\nQuestion {question_number}: {self.question_text}")
+        print(f"A. {self.choices.get('a', '')}")
+        print(f"B. {self.choices.get('b', '')}")
+        print(f"C. {self.choices.get('c', '')}")
+        print(f"D. {self.choices.get('d', '')}")
