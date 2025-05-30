71e027c65760797b04b133fca5c5f2fe62278bbf
Lance <enaganlance@gmail.com>
2025-05-30 21:50:10 +0800

    if the answer is correct method

--- quiz_program_to_oop/question.py
+++ quiz_program_to_oop/question.py
@@ -1,7 +1,8 @@
 class Question:
-    def __init__(self, question_text, choices):
+    def __init__(self, question_text, choices, correct_answer):
         self.question_text = question_text
         self.choices = choices
+        self.correct_answer = correct_answer.lower()
 
     def display_questions_choices(self, question_number):
         print(f"\nQuestion {question_number}: {self.question_text}")
@@ -9,3 +10,6 @@ class Question:
         print(f"B. {self.choices.get('b', '')}")
         print(f"C. {self.choices.get('c', '')}")
         print(f"D. {self.choices.get('d', '')}")
+
+    def is_correct(self, user_answer):
+        return user_answer.lower() == self.correct_answer.lower()
