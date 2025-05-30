a4512afaa27422825a265a36c3155def20c677d1
Lance <enaganlance@gmail.com>
2025-05-30 22:01:25 +0800

    display error if the file is not found

--- quiz_program_to_oop/quiz_import.py
+++ quiz_program_to_oop/quiz_import.py
@@ -1,4 +1,10 @@
+import os
+
 class QuizImport:
     def load_questions(filename):
         if not filename.endswith('.txt'):
             filename += '.txt'
+
+        if not os.path.exists(filename):
+            print(f"Error: File '{filename}' not found.")
+            return []
